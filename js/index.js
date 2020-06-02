2cab3dd4a56081c483f1b0f3216f4b2a5fc64b31
Galacs <34189296+Galacs@users.noreply.github.com>
2020-06-02 10:58:02 +0200

    Loging for benchmarking

--- js/index.js
+++ js/index.js
@@ -6,19 +6,24 @@ var todos = new Vue({
     methods: {
       DeleteToDo(todo){
         this.todos.splice(this.todos.indexOf(todo), 1);
+        console.log(todos.todos.length);
       }
     }
   })
 
 function addToDo(){
-    
-    todos.todos.push(prompt("Tache a faire:") );
+    todos.todos.push(prompt("Tache a faire:"));
+    console.log(todos.todos.length);
+}
+
+function ManualaddToDo(name, number = 1){
+  for (let i = 0; i < number; i++) {
+    todos.todos.push(name);
+  }
+  console.log(todos.todos.length);
 }
 
 function ClearTodos(){
     todos.todos = []
-}
-
-function DeleteToDo(element){
-  console.log(element);
+    console.log(todos.todos.length);
 }
